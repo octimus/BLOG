b253272aec3f4058aa923d21a5c0ca8c47e61a32
octimus <maoulidasaid74@gmail.com>
2020-01-12 10:53:12 +0300

    création du type Post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+
+export class Post {
+  title: string;  
+  content: string;  
+  loveIts: number;  
+  created_at: Date
+
+  constructor()
+  {
+    this.created_at = new Date();
+  }
+};
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +21,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+
+  public posts: Array<Post>;
+
+  constructor()
+  {
+    this.posts = [
+      {
+        title:"Post 0",
+        content: "Ceci est un premier post",
+        loveIts:0,
+        created_at: new Date()
+      },
+      {
+        title:"Post 1",
+        content: "Ceci est un deuxième post",
+        loveIts:1,
+        created_at: new Date()
+      },
+      {
+        title:"Post 2",
+        content: "Ceci est un troisième post",
+        loveIts:0,
+        created_at: new Date()
+      },
+    ]
+  }
 }
